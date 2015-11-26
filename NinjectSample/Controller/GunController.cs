54bd26cc323892434fafb3de659b39ef4bdd2349
Dennis Thinh Tan Nguyen <dttn@itu.dk>
2015-11-26 23:26:47 +0100

    Added dokumentation for GunController.

--- NinjectSample/Controller/GunController.cs
+++ NinjectSample/Controller/GunController.cs
@@ -7,9 +7,14 @@ namespace NinjectSample.Controller
 {
     public class GunController
     {
-
         private readonly IKernel _kernel;
         private IGun _selectedGun;
+
+        /// <summary>
+        ///     Constructor for the gun controller. It utalizes Ninject so one must specify a kernel to be
+        ///     used.
+        /// </summary>
+        /// <param name="kernel"> Some implementation of IKernel. </param>
         public GunController(IKernel kernel)
         {
             _kernel = kernel;
@@ -22,31 +27,41 @@ namespace NinjectSample.Controller
             }
         }
 
+        /// <summary>
+        ///     Sets the bindings that are to be used.
+        /// </summary>
         private void InitiaizeKernel()
         {
             _kernel.Bind<IGun>().To<PipeHandGun>();
             _kernel.Bind<IGun>().To<M16>();
         }
 
+        /// <summary>
+        ///     Let user select a gun of their choice
+        /// </summary>
         private void GunSelection()
         {
             Console.WriteLine("Select a gun: \n" +
-                            "1: Rifle\n" +
-                            "2: Pistol\n");
+                              "1: Rifle\n" +
+                              "2: Pistol\n");
             try
             {
                 var selection = int.Parse(Console.ReadLine());
 
-                //Ninjects finds the proper gun
+                //Ninjects returns an instace of the selected gun
                 switch (selection)
                 {
                     case 1:
                         Console.WriteLine("M16 combat rifle selected.");
-                        _selectedGun = _kernel.Get<M16>(); 
+                        _selectedGun = _kernel.Get<M16>();
                         break;
                     case 2:
                         Console.WriteLine("pipe pistol selected.");
                         _selectedGun = _kernel.Get<PipeHandGun>();
+                        break;
+                    default:
+                        Console.WriteLine("Please select a valid gun.");
+                        GunSelection();
                         break;
                 }
             }
@@ -57,11 +72,15 @@ namespace NinjectSample.Controller
             }
         }
 
+
+        /// <summary>
+        ///     perfom an actions specified by the user. Eg. Fire a gun.
+        /// </summary>
         private void GunAction()
         {
             Console.WriteLine("Gun Action: \n" +
-                            "1: Shoot\n" +
-                            "2: Reload\n");
+                              "1: Shoot\n" +
+                              "2: Reload\n");
             try
             {
                 var selection = int.Parse(Console.ReadLine());
@@ -74,17 +93,14 @@ namespace NinjectSample.Controller
                         Console.WriteLine(_selectedGun.Reload());
                         break;
                     default:
-                        Console.WriteLine("Invalid action."); 
+                        Console.WriteLine("Invalid action.");
                         break;
-
                 }
-
             }
             catch (FormatException)
             {
                 Console.WriteLine("Please select a valid action.");
             }
         }
-         
     }
 }
